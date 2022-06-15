d5ae51cd5d083cb2ef6ece94544c635124c67d47
Waleed Q <iamwaq@icloud.com>
2022-06-15 12:30:08 +0530

    Created the Project in STS for everyone - Waleed

---
+++ src/test/java/com/tcs/waleed/training/ProjectDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcs.waleed.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
